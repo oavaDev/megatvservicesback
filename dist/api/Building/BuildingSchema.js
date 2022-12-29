4774fb262c00e6a99a1b9a80bae037d962140b30
Omar Vega <omarvega598@gmail.com>
2022-12-28 23:57:51 -0500

    new structure

---
+++ dist/api/Building/BuildingSchema.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const BuildingSchema = new mongoose_1.Schema({
+    id: { type: String, required: false },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    postal: { type: String, required: true },
+    plans: [{}],
+}, { timestamps: true });
+const Building = (0, mongoose_1.model)('User', BuildingSchema);
+exports.default = Building;
