57352e62d4b166d1de7d1cc9d536254cece7d52c
Omar Vega <omarvega598@gmail.com>
2022-12-28 23:45:16 -0500

    add building schema

---
+++ api/models/Buildings.ts
@@ -0,0 +1,28 @@
+import { Schema, model, connect } from 'mongoose';
+
+interface IBuilding {
+  id?: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  postal: string;
+  plans: [{}];
+}
+
+const BuildingSchema = new Schema<IBuilding>(
+  {
+    id: { type: String, required: false },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    postal: { type: String, required: true },
+    plans: [{}],
+  },
+  { timestamps: true }
+);
+
+const Building = model<IBuilding>('User', BuildingSchema);
+
+export default Building;
