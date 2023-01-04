139e000fc73485999fc6c9266af5e611a62b5ad1
Omar Vega <omarvega598@gmail.com>
2023-01-04 10:41:32 -0500

    added new d

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 const express_1 = __importDefault(require("express"));
 const express_2 = __importDefault(require("./express"));
 const database_1 = require("./database");
+const routes_config_1 = __importDefault(require("./routes.config"));
 const app = (0, express_1.default)();
 app.listen(process.env.APP_PORT || 8080, () => {
     (0, express_2.default)(app);
     (0, database_1.connect)();
+    (0, routes_config_1.default)(app);
     console.log(`server listening on http://localhost:${process.env.APP_PORT || 8080}`);
 });
 exports.default = app;
--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ require('dotenv').config();
 import express, { Express, Request, Response } from 'express';
 import expressConfig from './express';
 import { connect } from './database';
+import routes from './routes.config';
 const app: Express = express();
 
 app.listen(process.env.APP_PORT || 8080, () => {
   expressConfig(app);
   connect();
+  routes(app);
   console.log(
     `server listening on http://localhost:${process.env.APP_PORT || 8080}`
   );
